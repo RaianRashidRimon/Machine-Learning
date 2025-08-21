64f4440bded40d389936d9842f107b183c53ca2f
Raian Rashid <rimonraian2@gmail.com>
2025-08-21 18:56:07 +0600

    bar line bokeh 1

--- Graph Codes/Bar and Line Graphs/Bokeh 1.py
+++ Graph Codes/Bar and Line Graphs/Bokeh 1.py
@@ -2,14 +2,14 @@ from bokeh.plotting import figure, show
 from bokeh.layouts import gridplot
 from bokeh.models import ColumnDataSource
 
-# Data
+
 months = ['March', 'April', 'May', 'June', 'July', 'August']
 cumulative = [120, 170, 280, 380, 430, 450]
 
-# Create data source
 source = ColumnDataSource(data=dict(months=months, cumulative=cumulative))
 
-# Create figure for bar graph
+
+
 bar_fig = figure(width=400, height=400,
                  title="Cumulative Earnings (Bar Graph)",
                  x_axis_label='Month',
@@ -17,7 +17,9 @@ bar_fig = figure(width=400, height=400,
                  x_range=months)
 bar_fig.vbar(x='months', top='cumulative', width=0.5, source=source, fill_color='lightblue', line_color='black')
 
-# Create figure for line graph
+
+
+
 line_fig = figure(width=400, height=400,
                   title="Cumulative Earnings (Line Graph)",
                   x_axis_label='Month',
@@ -27,7 +29,7 @@ line_fig.line('months', 'cumulative', source=source, color='green', line_width=2
 line_fig.scatter('months', 'cumulative', source=source, size=8, color='green', legend_label='Data Points')
 line_fig.legend.click_policy="hide"
 
-# Arrange figures in a 1x2 grid
+
 grid = gridplot([[bar_fig, line_fig]])
 
 show(grid)
