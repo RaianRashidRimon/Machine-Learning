9d224f1a0020d61fe7d6f40833e09c373eb49adb
Raian Rashid <rimonraian2@gmail.com>
2025-08-21 18:53:58 +0600

    bar bokeh 1

--- Graph Codes/Bar Graphs/Bokeh 1.py
+++ Graph Codes/Bar Graphs/Bokeh 1.py
@@ -1,17 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 from bokeh.plotting import figure, show
 from bokeh.io import output_notebook
 
-# Data
+
 fruits = ['Apple', 'Orange', 'Banana', 'Kiwifruit', 'Blueberry', 'Grapes']
 people = [35, 30, 10, 25, 40, 5]
 
-# Create figure
+
 p = figure(x_range=fruits, title="Survey Results: Nicest Fruit", toolbar_location=None, tools="")
 p.vbar(x=fruits, top=people, width=0.9, color="skyblue")
 
@@ -19,13 +13,9 @@ p.xaxis.axis_label = "Fruit"
 p.yaxis.axis_label = "Number of People"
 p.xaxis.major_label_orientation = "vertical"
 
-# Show plot
+
+
+
+
 output_notebook()
 show(p)
-
-
-# In[ ]:
-
-
-
-
