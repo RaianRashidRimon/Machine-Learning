8d1a6eaadab2fa655884345387d8cb8c898abff1
Raian Rashid <rimonraian2@gmail.com>
2025-08-23 00:07:42 +0600

    bar matplotlib 2

--- Graph Codes/Bar Graphs/Matplotlib 2.py
+++ Graph Codes/Bar Graphs/Matplotlib 2.py
@@ -1,10 +1,6 @@
 import matplotlib.pyplot as plt
-
-# Data
 grades = ['A', 'B', 'C', 'D']
 students = [4, 12, 10, 2]
-
-# Plot
 plt.bar(grades, students, color='lightgreen')
 plt.xlabel('Grade')
 plt.ylabel('Number of Students')
