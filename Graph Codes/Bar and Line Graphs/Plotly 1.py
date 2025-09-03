8a429c70c505c6a34cd8a8a2bf169f82b910c577
Raian Rashid <rimonraian2@gmail.com>
2025-09-04 00:08:33 +0600

    bar line plotly 1

--- Graph Codes/Bar and Line Graphs/Plotly 1.py
+++ Graph Codes/Bar and Line Graphs/Plotly 1.py
@@ -1,22 +1,15 @@
 import plotly.graph_objects as go
 import numpy as np
 
-# Data
 months = ['March', 'April', 'May', 'June', 'July', 'August']
 cumulative = [120, 170, 280, 380, 430, 450]
-
-# Create figure with both bar and line traces
 fig = go.Figure()
-
-# Add cumulative bar graph
 fig.add_trace(go.Bar(
     x=months,
     y=cumulative,
     name='Cumulative Earnings (Bar)',
     marker=dict(color='lightblue', line=dict(color='black', width=1))
 ))
-
-# Add cumulative line graph
 fig.add_trace(go.Scatter(
     x=months,
     y=cumulative,
@@ -25,7 +18,6 @@ fig.add_trace(go.Scatter(
     marker=dict(color='green', size=8),
     line=dict(color='green', width=2)
 ))
-
 fig.update_layout(
     title='Cumulative Earnings by Month',
     xaxis_title='Month',
@@ -34,11 +26,16 @@ fig.update_layout(
     showlegend=True
 )
 
-# Add grid lines
+
+
+
 fig.update_xaxes(showgrid=True)
 fig.update_yaxes(showgrid=True)
-
-# Rotate x-axis labels for readability
 fig.update_layout(xaxis=dict(tickangle=45))
+
+
+
+
+
 
 fig.show()
