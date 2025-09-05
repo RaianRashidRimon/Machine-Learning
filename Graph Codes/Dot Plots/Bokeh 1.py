47752cbe6087046ab182c270140340861fb88037
Raian Rashid <rimonraian2@gmail.com>
2025-09-06 01:52:55 +0600

    dot plot bokeh 1

--- Graph Codes/Dot Plots/Bokeh 1.py
+++ Graph Codes/Dot Plots/Bokeh 1.py
@@ -1,26 +1,24 @@
 from bokeh.plotting import figure, show
 from bokeh.models import ColumnDataSource
 
-# Data
 minutes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 people = [6, 2, 3, 5, 2, 5, 0, 0, 2, 3, 7, 4, 1]
-
-# Create data source
 source = ColumnDataSource(data=dict(minutes=minutes, people=people))
-
-# Create figure
 p = figure(width=600, height=400, 
           title="Time to Eat Breakfast Survey",
           x_axis_label='Minutes',
           y_axis_label='Number of People')
-
-# Add dots
 p.circle('minutes', 'people', size=10, source=source, alpha=0.6)
 
-# Customize
+
+
+
 p.xgrid.grid_line_color = None
 p.ygrid.grid_line_color = "gray"
 p.ygrid.grid_line_dash = [6, 4]
-p.xaxis.ticker = minutes  # Show all minute values
+p.xaxis.ticker = minutes 
+
+
+
 
 show(p)
