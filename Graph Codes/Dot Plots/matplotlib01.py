5ce409826e68846bf8104c12d9d19fb786ff49ae
Raian Rashid <rimonraian2@gmail.com>
2025-09-08 13:46:17 +0600

    dot plot matplotlib 1

---
+++ Graph Codes/Dot Plots/matplotlib01.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+# Data
+minutes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+people = [6, 2, 3, 5, 2, 5, 0, 0, 2, 3, 7, 4, 1]
+
+# Create dot plot
+plt.figure(figsize=(10, 6))
+plt.scatter(minutes, people, s=100, alpha=0.6)
+plt.title('Time to Eat Breakfast Survey')
+plt.xlabel('Minutes')
+plt.ylabel('Number of People')
+plt.grid(True, linestyle='--', alpha=0.7)
+plt.xticks(minutes)  # Show all minute values on x-axis
+plt.show()
