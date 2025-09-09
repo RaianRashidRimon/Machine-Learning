20c8e124bda1a510d9bbe770746a89599c65bedd
Raian Rashid <rimonraian2@gmail.com>
2025-09-10 00:11:10 +0600

    dot plot plotly 1

--- Graph Codes/Dot Plots/Plotly 1.py
+++ Graph Codes/Dot Plots/Plotly 1.py
@@ -1,10 +1,13 @@
 import plotly.graph_objects as go
 
-# Data
 minutes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 people = [6, 2, 3, 5, 2, 5, 0, 0, 2, 3, 7, 4, 1]
 
-# Create dot plot
+
+
+
+
+
 fig = go.Figure(data=[go.Scatter(
     x=minutes,
     y=people,
@@ -13,18 +16,13 @@ fig = go.Figure(data=[go.Scatter(
     text=[f'{p} people' for p in people],
     hoverinfo='x+y+text'
 )])
-
 fig.update_layout(
     title='Time to Eat Breakfast Survey',
     xaxis_title='Minutes',
     yaxis_title='Number of People',
     xaxis=dict(tickmode='array', tickvals=minutes),
-    # Removed gridwidth - we can control grid through other means if needed
     showlegend=False
 )
-
-# Add grid lines
 fig.update_xaxes(showgrid=True)
 fig.update_yaxes(showgrid=True)
-
 fig.show()
