cde6f2a8dd59822d45e2cb277d10b33abac4ae6a
Raian Rashid <rimonraian2@gmail.com>
2025-09-13 01:29:34 +0600

    dot plot seaborn 2

--- Graph Codes/Dot Plots/Seaborn 2.py
+++ Graph Codes/Dot Plots/Seaborn 2.py
@@ -1,11 +1,13 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Data
 percent_access = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 countries = [5, 6, 12, 5, 4, 5, 6, 10, 15, 34]
 
-# Create dot plot with seaborn styling
+
+
+
+
 plt.figure(figsize=(10, 6))
 sns.scatterplot(x=percent_access, y=countries, size=100, sizes=(100, 100), alpha=0.6)
 plt.title('Access to Electricity by Country')
