b82b00aa0cdde9031edb41f3292007b3b24e04c7
Raian Rashid <rimonraian2@gmail.com>
2025-09-16 00:10:15 +0600

    histogram matplotlib 1

--- Graph Codes/Histogram/Matplotlib 1.py
+++ Graph Codes/Histogram/Matplotlib 1.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 
-# Data
-bins = [100, 150, 200, 250, 300, 350]  # Bin edges
-frequencies = [5, 12, 15, 10, 3]  # Example frequencies
+bins = [100, 150, 200, 250, 300, 350] 
+frequencies = [5, 12, 15, 10, 3]  
 
-# Create histogram
+
+
+
 plt.figure(figsize=(10, 6))
 plt.hist(bins[:-1], bins, weights=frequencies, edgecolor='black')
 plt.title('Tree Heights in Orchard')
