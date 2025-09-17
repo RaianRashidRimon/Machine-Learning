7ad63cb8cf078d6a330a0db796cfec2761debeae
Raian Rashid <rimonraian2@gmail.com>
2025-09-18 00:14:53 +0600

    histogram plotly 1

--- Graph Codes/Histogram/Plotly 1.py
+++ Graph Codes/Histogram/Plotly 1.py
@@ -1,25 +1,26 @@
 import plotly.graph_objects as go
 
-# Data
-bins = [100, 150, 200, 250, 300, 350]  # Bin edges
-frequencies = [5, 12, 15, 10, 3]  # Example frequencies
 
-# Create histogram
+bins = [100, 150, 200, 250, 300, 350] 
+frequencies = [5, 12, 15, 10, 3]  
+
+
+
+
+
 fig = go.Figure(data=[go.Bar(
-    x=[125, 175, 225, 275, 325],  # Center of each bin
+    x=[125, 175, 225, 275, 325],  
     y=frequencies,
-    width=50,  # Width of bars
+    width=50,
     marker=dict(line=dict(color='black', width=1))
 )])
-
 fig.update_layout(
     title='Tree Heights in Orchard',
     xaxis_title='Height (cm)',
     yaxis_title='Number of Trees',
-    bargap=0,  # No gap between bars
+    bargap=0,  
     xaxis=dict(tickmode='array', tickvals=bins),
     showlegend=False
 )
-
 fig.update_yaxes(showgrid=True)
 fig.show()
