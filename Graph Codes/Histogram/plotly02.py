833298eee8b84bb2893866dd2cff01b2c807447b
Raian Rashid <rimonraian2@gmail.com>
2025-09-19 01:41:52 +0600

    histogram plotly 2

---
+++ Graph Codes/Histogram/plotly02.py
@@ -0,0 +1,27 @@
+import plotly.graph_objects as go
+
+# Data
+weight_gains = [-0.2, 0, 0.1, 0.1, 0.3, 0.4, 0.5, 0.5, 0.6, 1.6]
+bins = [-0.5, 0, 0.5, 1.0, 1.5, 2.0]
+
+# Create histogram
+fig = go.Figure(data=[go.Histogram(
+    x=weight_gains,
+    xbins=dict(start=-0.5, end=2.0, size=0.5),
+    marker=dict(
+        color='green',  # Changed to green
+        line=dict(color='black', width=1)
+    )
+)])
+
+fig.update_layout(
+    title='Pup Weight Gain per Month',
+    xaxis_title='Weight Gain (kg)',
+    yaxis_title='Number of Months',
+    xaxis=dict(tickmode='array', tickvals=bins),
+    bargap=0,
+    showlegend=False
+)
+
+fig.update_yaxes(showgrid=True)
+fig.show()
