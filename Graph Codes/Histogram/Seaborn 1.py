862d2ec1254e1725e193264f69a2d5d0fba218ae
Raian Rashid <rimonraian2@gmail.com>
2025-09-21 00:31:21 +0600

    histogram seaborn 1

--- Graph Codes/Histogram/Seaborn 1.py
+++ Graph Codes/Histogram/Seaborn 1.py
@@ -2,11 +2,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Generate data points based on assumed frequencies
+
+
 heights = (np.array([125] * 5 + [175] * 12 + [225] * 15 + [275] * 10 + [325] * 3))
 bins = [100, 150, 200, 250, 300, 350]
 
-# Create histogram with seaborn styling
+
+
+
 plt.figure(figsize=(10, 6))
 sns.histplot(data=heights, bins=bins, edgecolor='black')
 plt.title('Tree Heights in Orchard')
