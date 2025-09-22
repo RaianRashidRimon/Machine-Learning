a0b1da762878c3fdc754f586a0055f8cc28296df
Raian Rashid <rimonraian2@gmail.com>
2025-09-22 14:31:03 +0600

    histogram seaborn 2

--- Graph Codes/Histogram/Seaborn 2.py
+++ Graph Codes/Histogram/Seaborn 2.py
@@ -1,13 +1,16 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Data
+
+
+
 weight_gains = [-0.2, 0, 0.1, 0.1, 0.3, 0.4, 0.5, 0.5, 0.6, 1.6]
 bins = [-0.5, 0, 0.5, 1.0, 1.5, 2.0]
 
-# Create histogram with seaborn styling
+
+
 plt.figure(figsize=(10, 6))
-sns.histplot(data=weight_gains, bins=bins, edgecolor='green', color='green')  # Changed to green
+sns.histplot(data=weight_gains, bins=bins, edgecolor='green', color='green') 
 plt.title('Pup Weight Gain per Month')
 plt.xlabel('Weight Gain (kg)')
 plt.ylabel('Number of Months')
