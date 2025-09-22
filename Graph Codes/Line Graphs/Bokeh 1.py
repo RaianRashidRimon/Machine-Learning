fe10f9e9f233efda4e846540c8d535fdc67c52f8
Raian Rashid <rimonraian2@gmail.com>
2025-09-23 00:38:00 +0600

    line bokeh 1

--- Graph Codes/Line Graphs/Bokeh 1.py
+++ Graph Codes/Line Graphs/Bokeh 1.py
@@ -1,27 +1,22 @@
 from bokeh.plotting import figure, show
 from bokeh.models import ColumnDataSource
 
-# Data
+
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 sales = [410, 440, 550, 420, 610, 790, 770]
 
-# Create data source
 source = ColumnDataSource(data=dict(days=days, sales=sales))
-
-# Create figure
 p = figure(width=600, height=400,
           title="Ice Cream Sales by Day",
           x_axis_label='Day of Week',
           y_axis_label='Sales ($)',
-          x_range=days)  # Set categorical x-axis
-
-# Add line and circles
+          x_range=days)
 p.line('days', 'sales', source=source, line_width=2, color='blue')
 p.circle('days', 'sales', source=source, size=8, fill_color='white', line_color='blue')
 
-# Customize
+
+
 p.xgrid.grid_line_color = None
 p.ygrid.grid_line_color = "gray"
 p.ygrid.grid_line_dash = [6, 4]
-
 show(p)
