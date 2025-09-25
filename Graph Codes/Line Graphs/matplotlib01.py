652f28e7ea6aab9d0ae2dfcd4ac811c75a296a8c
Raian Rashid <rimonraian2@gmail.com>
2025-09-26 02:14:36 +0600

    line matplotlib 1

---
+++ Graph Codes/Line Graphs/matplotlib01.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+# Data
+days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+sales = [410, 440, 550, 420, 610, 790, 770]
+
+# Create line graph
+plt.figure(figsize=(10, 6))
+plt.plot(days, sales, marker='o', linestyle='-', color='b')
+plt.title('Ice Cream Sales by Day')
+plt.xlabel('Day of Week')
+plt.ylabel('Sales ($)')
+plt.grid(True, linestyle='--', alpha=0.7)
+plt.show()
