244981f9ff76e2894d1658519b211d3954b16ab1
Raian Rashid <rimonraian2@gmail.com>
2025-09-27 00:55:16 +0600

    line plotly\ 1

--- Graph Codes/Line Graphs/Plotly 1.py
+++ Graph Codes/Line Graphs/Plotly 1.py
@@ -1,10 +1,10 @@
 import plotly.graph_objects as go
 
-# Data
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 sales = [410, 440, 550, 420, 610, 790, 770]
 
-# Create line graph
+
+
 fig = go.Figure(data=[go.Scatter(
     x=days,
     y=sales,
@@ -13,16 +13,12 @@ fig = go.Figure(data=[go.Scatter(
     hoverinfo='x+y',
     line=dict(color='blue')
 )])
-
 fig.update_layout(
     title='Ice Cream Sales by Day',
     xaxis_title='Day of Week',
     yaxis_title='Sales ($)',
     showlegend=False
 )
-
-# Add grid lines
 fig.update_xaxes(showgrid=True)
 fig.update_yaxes(showgrid=True)
-
 fig.show()
