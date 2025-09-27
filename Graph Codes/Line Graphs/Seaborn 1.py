91c6dcb827062d538e631c0ca6e58786e80c0405
Raian Rashid <rimonraian2@gmail.com>
2025-09-28 01:18:51 +0600

    line seaborn 1

--- Graph Codes/Line Graphs/Seaborn 1.py
+++ Graph Codes/Line Graphs/Seaborn 1.py
@@ -1,11 +1,11 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Data
 days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 sales = [410, 440, 550, 420, 610, 790, 770]
 
-# Create line graph with seaborn styling
+
+
 plt.figure(figsize=(10, 6))
 sns.lineplot(x=days, y=sales, marker='o')
 plt.title('Ice Cream Sales by Day')
