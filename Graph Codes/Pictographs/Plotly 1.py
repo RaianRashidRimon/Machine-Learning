6eeb1db2baa5829f712103349e6311312828df11
Raian Rashid <rimonraian2@gmail.com>
2025-10-29 12:08:52 +0600

    pictograph plotly 1

--- Graph Codes/Pictographs/Plotly 1.py
+++ Graph Codes/Pictographs/Plotly 1.py
@@ -1,14 +1,10 @@
 import plotly.graph_objects as go
 
-# Data
 months = ['January', 'February', 'March', 'April']
 apples = [10, 40, 25, 20]
 apple_counts = [a / 10 for a in apples]
 
-# Text labels for apple symbols
 apple_labels = ['🍎' * int(c) + ('½🍎' if c % 1 >= 0.5 else '') for c in apple_counts]
-
-# Create bar plot
 fig = go.Figure(data=[go.Bar(
     x=months,
     y=apple_counts,
@@ -16,7 +12,6 @@ fig = go.Figure(data=[go.Bar(
     textposition='auto',
     marker=dict(color='red')
 )])
-
 fig.update_layout(
     title='Apple Sales by Month',
     xaxis_title='Month',
@@ -24,6 +19,5 @@ fig.update_layout(
     yaxis=dict(range=[0, 5]),
     showlegend=False
 )
-
 fig.update_yaxes(showgrid=True)
 fig.show()
