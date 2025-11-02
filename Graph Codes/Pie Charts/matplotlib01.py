00d67724ac396e0c9a24d7613d75666cd7930008
Raian Rashid <rimonraian2@gmail.com>
2025-11-02 12:55:03 +0600

    piechart matplotlib 1

--- Graph Codes/Pie Charts/matplotlib01.py
+++ Graph Codes/Pie Charts/matplotlib01.py
@@ -1,11 +1,7 @@
 import matplotlib.pyplot as plt
-
-# Data
 movies = ['Comedy', 'Action', 'Romance', 'Drama', 'SciFi']
 votes = [4, 5, 6, 1, 4]
-
-# Plot
 plt.pie(votes, labels=movies, autopct='%1.1f%%', startangle=90, colors=['#ff9999','#66b3ff','#99ff99','#ffcc99','#c2c2f0'])
 plt.title('Favorite Type of Movie')
-plt.axis('equal')  # Equal aspect ratio ensures that pie chart is circular.
+plt.axis('equal') 
 plt.show()
