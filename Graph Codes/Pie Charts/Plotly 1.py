66980bc22f2ec65b1b6201f898ce57ceadf0d8a0
Raian Rashid <rimonraian2@gmail.com>
2025-11-15 22:10:02 +0600

    piechart plotly 1

--- Graph Codes/Pie Charts/Plotly 1.py
+++ Graph Codes/Pie Charts/Plotly 1.py
@@ -1,10 +1,7 @@
 import plotly.graph_objects as go
 
-# Data
 movies = ['Comedy', 'Action', 'Romance', 'Drama', 'SciFi']
 votes = [4, 5, 6, 1, 4]
-
-# Plot
 fig = go.Figure(data=[go.Pie(labels=movies, values=votes, hole=0.3)])
 fig.update_layout(title='Favorite Type of Movie')
 fig.show()
