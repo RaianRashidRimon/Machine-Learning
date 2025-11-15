64732a652ed746490c72a9ad150faad976539556
Raian Rashid <rimonraian2@gmail.com>
2025-11-15 22:09:40 +0600

    piechart matplotlib 2

--- Graph Codes/Pie Charts/Matplotlib 2.py
+++ Graph Codes/Pie Charts/Matplotlib 2.py
@@ -1,12 +1,9 @@
 import matplotlib.pyplot as plt
 
-# Data
 grades = ['A', 'B', 'C', 'D']
 counts = [4, 12, 10, 2]
-
-# Create pie chart
 plt.figure(figsize=(8, 8))
 plt.pie(counts, labels=grades, autopct='%1.1f%%', startangle=90)
 plt.title('Grade Distribution in Recent Test')
-plt.axis('equal')  # Equal aspect ratio ensures pie is drawn as a circle
+plt.axis('equal')  
 plt.show()
