3ea0515e09c5aa2079a9f631358437a89007e77c
Raian Rashid <rimonraian2@gmail.com>
2025-11-17 19:17:34 +0600

    piechart seaborn 1

---
+++ Graph Codes/Pie Charts/seaborn01.py
@@ -0,0 +1,12 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Data
+movies = ['Comedy', 'Action', 'Romance', 'Drama', 'SciFi']
+votes = [4, 5, 6, 1, 4]
+
+# Plot
+plt.pie(votes, labels=movies, autopct='%1.1f%%', startangle=90, colors=['#ff9999','#66b3ff','#99ff99','#ffcc99','#c2c2f0'])
+plt.title('Favorite Type of Movie')
+plt.axis('equal')
+plt.show()
