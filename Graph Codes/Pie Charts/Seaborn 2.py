81eb75523f9775cf0f3ec334e46479757ff81ccd
Raian Rashid <rimonraian2@gmail.com>
2025-11-20 01:38:21 +0600

    piechart seaborn 2

--- Graph Codes/Pie Charts/Seaborn 2.py
+++ Graph Codes/Pie Charts/Seaborn 2.py
@@ -1,14 +1,10 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Set seaborn style
-sns.set_style("whitegrid")
 
-# Data
+sns.set_style("whitegrid")a
 grades = ['A', 'B', 'C', 'D']
 counts = [4, 12, 10, 2]
-
-# Create pie chart with seaborn color palette
 plt.figure(figsize=(8, 8))
 plt.pie(counts, labels=grades, autopct='%1.1f%%', startangle=90,
         colors=sns.color_palette("pastel"))
