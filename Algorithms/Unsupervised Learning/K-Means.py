39963960e2b9998f05a10ea214fa8809f0634eb4
Raian Rashid <rimonraian2@gmail.com>
2025-11-23 01:02:00 +0600

    kmeans algo

--- Algorithms/Unsupervised Learning/K-Means.py
+++ Algorithms/Unsupervised Learning/K-Means.py
@@ -3,18 +3,19 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
 
-# Generate synthetic dataset
-X, _ = make_blobs(n_samples=300, centers=4, cluster_std=1.0, random_state=42)
 
-# Apply K-Means clustering
+
+
+X, _ = make_blobs(n_samples=300, centers=4, cluster_std=1.0, random_state=42)
 kmeans = KMeans(n_clusters=4, random_state=42, n_init=10)
 kmeans.fit(X)
-
-# Cluster centers and labels
 centers = kmeans.cluster_centers_
 labels = kmeans.labels_
 
-# Plot the clusters
+
+
+
+
 plt.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis', alpha=0.6)
 plt.scatter(centers[:, 0], centers[:, 1], c='red', marker='X', s=200, label='Centroids')
 plt.title('K-Means Clustering')
