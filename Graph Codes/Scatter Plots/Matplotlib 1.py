330b63834f1aa9b893793db9d83bf8f6c22faa24
Raian Rashid <rimonraian2@gmail.com>
2025-11-28 23:48:48 +0600

    scatter plot matplotlib 1

--- Graph Codes/Scatter Plots/Matplotlib 1.py
+++ Graph Codes/Scatter Plots/Matplotlib 1.py
@@ -2,19 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-# Data
+
 temperatures = np.array([14.2, 16.4, 11.9, 15.2, 18.5, 22.1, 19.4, 25.1, 23.4, 18.1, 22.6, 17.2]).reshape(-1, 1)
 sales = np.array([215, 325, 185, 332, 406, 522, 412, 614, 544, 421, 445, 408])
 
-# Fit linear regression
+
+
 model = LinearRegression()
 model.fit(temperatures, sales)
-
-# Predict values for the line
 temp_range = np.linspace(min(temperatures), max(temperatures), 100).reshape(-1, 1)
 sales_pred = model.predict(temp_range)
-
-# Create scatter plot
 plt.figure(figsize=(10, 6))
 plt.scatter(temperatures, sales, color='blue', label='Data Points')
 plt.plot(temp_range, sales_pred, color='red', label='Best-Fit Line')
@@ -25,5 +22,7 @@ plt.grid(True, linestyle='--', alpha=0.7)
 plt.legend()
 plt.show()
 
-# Print the equation of the line (optional)
+
+
+
 print(f'Best-Fit Line: y = {model.coef_[0]:.2f}x + {model.intercept_:.2f}')
