58c0c6e9fafbef271d128e4a669989f5d950d812
Raian Rashid <rimonraian2@gmail.com>
2026-02-09 23:34:13 +0600

    xgboost

--- Algorithms/Supervised Learning/XGBoost.py
+++ Algorithms/Supervised Learning/XGBoost.py
@@ -1,26 +1,35 @@
-from xgboost import XGBClassifier
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, ConfusionMatrixDisplay, classification_report
+from xgboost import XGBClassifier
 
-X, y = load_iris(return_X_y=True)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X, y = load_wine(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=42, stratify=y)
 
 model = XGBClassifier(
     n_estimators=100,
     learning_rate=0.1,
-    max_depth=3,
+    max_depth=4,
     eval_metric='mlogloss',
-    random_state=42
-)
+    random_state=42,
+    n_jobs=-1)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
-
-
 print("Accuracy:", accuracy_score(y_test, y_pred))
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
 
-ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
-plt.title("Confusion Matrix - XGBoost")
+ConfusionMatrixDisplay.from_predictions(
+    y_test,
+    y_pred,
+    cmap='Blues',
+    values_format='d')
+
+
+plt.title("Confusion Matrix")
 plt.show()
